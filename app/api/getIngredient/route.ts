7cf36c974b55c1e071144a23c867b9bda72ab605
Ricky Tran <therichardtran@gmail.com>
2025-01-07 13:41:01 -0700

    Add more error logging

--- app/api/getIngredient/route.ts
+++ app/api/getIngredient/route.ts
@@ -5,16 +5,22 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const query = searchParams.get('query');
 
+    // Log environment variables (for debugging purposes only)
+    console.log("EDAMAM_APP_ID:", process.env.EDAMAM_APP_ID);
+    console.log("EDAMAM_API_KEY:", process.env.EDAMAM_API_KEY);
+
     const response = await fetch(
       `https://api.edamam.com/api/food-database/v2/parser?ingr=${query}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}&nutrition-type=cooking`
     );
 
     if (!response.ok) {
-      throw new Error("Failed to fetch data from Edamam API");
+      const errorText = await response.text();
+      console.error("Failed to fetch data from Edamam API:", response.status, errorText);
+      throw new Error(`Failed to fetch data from Edamam API: ${response.status} ${errorText}`);
     }
 
     const result = await response.json();
-    console.log("Fetched result from edamam API:", result); // Log the fetched result     
+    console.log("Fetched result from Edamam API:", result); // Log the fetched result
 
     // Ensure the result is a valid JSON object
     if (typeof result !== 'object' || result === null) {
